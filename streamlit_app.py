f8c8fa09c3f61902b38925026b03244497ea865d
ksbalas <40187856+ksbalas@users.noreply.github.com>
2022-07-29 16:55:52 -0700

    bug fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,13 +10,13 @@ streamlit.text("Hello! My Name is Bala Sellamuthu")
 fruit_csv = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 #Indexing
-fruit_csv = simple_csv.set_index("Fruit")
+fruit_csv = fruit_csv.set_index("Fruit")
 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(fruit_csv.index),['Grapes', 'Apple','Avocado','Strawberries'])
 
 #Filter fruits based on above list
 fruits_to_show  = fruit_csv.loc(fruits_selected)
-streamlit.dataframe(simple_csv)
+streamlit.dataframe(fruit_csv)
 
 #Filtered list 
 streamlit.dataframe(fruits_to_show) 

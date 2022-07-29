58dee52eb6f2e75945828c6b7708c506e1ebff65
ksbalas <40187856+ksbalas@users.noreply.github.com>
2022-07-29 16:57:21 -0700

    bug fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ fruit_csv = fruit_csv.set_index("Fruit")
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(fruit_csv.index),['Grapes', 'Apple','Avocado','Strawberries'])
 
 #Filter fruits based on above list
-fruits_to_show  = fruit_csv.loc(fruits_selected)
+fruits_to_show  = fruit_csv.loc[fruits_selected]
 streamlit.dataframe(fruit_csv)
 
 #Filtered list 

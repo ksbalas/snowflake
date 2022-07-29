462df6934c714372c4b71a5cd43bce81e6ce93ae
ksbalas <40187856+ksbalas@users.noreply.github.com>
2022-07-29 16:43:30 -0700

    Added multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,6 @@ streamlit.text("Hello! My Name is Bala Sellamuthu")
 
 simple_csv = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(simple_csv)
+
+#Multiselect 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))

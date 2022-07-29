a7e3bc5a49056de6f78366a6002d4bc413313488
ksbalas <40187856+ksbalas@users.noreply.github.com>
2022-07-29 16:42:00 -0700

    Added Pandas Library and streamlit dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 #Sample File
 import streamlit
+import pandas
 
 streamlit.title("KS Bala's Streamlit App")
 
 streamlit.header("Heading")
 streamlit.text("Hello! My Name is Bala Sellamuthu")
+
+simple_csv = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(simple_csv)

3b36c83afb7a866f9907687451820aa04c2b9eee
ksbalas <40187856+ksbalas@users.noreply.github.com>
2022-07-29 16:45:28 -0700

    Changed order of multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ streamlit.header("Heading")
 streamlit.text("Hello! My Name is Bala Sellamuthu")
 
 simple_csv = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.multiselect("Pick some fruits:", list(simple_csv.index))
 streamlit.dataframe(simple_csv)
 
-#Multiselect 
-streamlit.multiselect("Pick some fruits:", list(simple_csv.index))
+
